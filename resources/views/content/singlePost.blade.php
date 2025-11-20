7f9b3c18d4a6d32ba62abed44545a78a5f0d94e3
iamfizzer <bagindafizzer99@gmai.com>
2025-11-20 14:00:19 +0700

    ganti berita 2

--- resources/views/content/singlePost.blade.php
+++ resources/views/content/singlePost.blade.php
@@ -25,7 +25,9 @@
                             <div class="mb-2 px-2">{{ $author }}</div>
                         </div>
                     </div>
-                    <img src="<?= asset('assets/img/bg-img/') ?>/{{ $thumbnail }}" alt="" width="500px">
+                    <center>
+                        <img src="<?= asset('assets/img/bg-img/') ?>/{{ $thumbnail }}" alt="" width="500px">
+                    </center>
                 </div>
 
             </div>
